be8c377c9a5a776c6a723cb122f6815c49065234
jasonkalmeida <jasonkalmeida@gmail.com>
2016-02-19 19:44:51 -0500

    WWOTS

---
+++ bootstrap/js/podcast.js
@@ -0,0 +1,20 @@
+function showScroll() {
+    $('html, body').animate({
+        scrollTop: $("#show").offset().top
+    }, 400);
+};
+
+function aboutScroll() {
+
+	    $('html, body').animate({
+	        scrollTop: $("#about").offset().top
+	    }, 400);
+
+};
+
+function contactScroll() {
+
+	    $('html, body').animate({
+	        scrollTop: $("#contact").offset().top
+	    }, 400);
+};
