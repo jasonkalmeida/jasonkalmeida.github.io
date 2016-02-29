1e3a013527292258629700cc0a4f88cc2ea897d2
jasonkalmeida <jasonkalmeida@gmail.com>
2016-02-29 14:27:49 -0500

    Update For Loader

--- bootstrap/js/podcast.js
+++ bootstrap/js/podcast.js
@@ -1,3 +1,5 @@
+
+
 function showScroll() {
     $('html, body').animate({
         scrollTop: $("#show").offset().top
@@ -21,9 +23,54 @@ function contactScroll() {
 
 function subscribed()
 {
-	$(document).ready(function(){
-	  $('#mc-embedded-subscribe-form').submit(function(){
-	    $('#email').attr('placeholder','Thank you for subscribing!');
-	  });
-	});
 }
+
+
+$(function() {
+  $( "#button" ).click(function() {
+	  
+	  var el = document.getElementById("button");
+	     if (el.innerHTML == "SUBMIT") 
+	     {
+	         el.innerHTML = '<div class="spinner"></div>';
+	     }
+		 
+      setTimeout(function() {
+        
+		  var el = document.getElementById("button");
+		     if (el.innerHTML == '<div class="spinner"></div>') 
+		     {
+		         el.innerHTML = '&#xf00c;';
+		     }
+			 
+			 $( "#button" ).addClass( "change", 450);
+			 
+			 $('#email').attr('placeholder','Thank you for subscribing!');
+		    
+		
+         }, 3250 );
+
+
+	//$( "#button" ).removeClass( "btn btn-default" );
+	
+   
+	
+    //$( "#button" ).addClass( "change", 450);
+	//callback();
+  });
+ 
+/*
+    function callback() {
+		
+	    
+      setTimeout(function() {
+        $( "#button" ).removeClass( "change" );
+		$( "#button" ).addClass( "btn btn-default" );
+		
+  	  var el = document.getElementById("button");
+
+  	       el.innerHTML = 'SUBMIT';
+		
+      }, 4250 );
+    }*/
+  });
